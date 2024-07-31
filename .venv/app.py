1b11f3fa7a70cd9c14f110f377a2343657081d45
Parsaa <parsaamanes@yahoo.com>
2024-07-31 14:42:57 +0300

    last stages

--- .venv/app.py
+++ .venv/app.py
@@ -2,25 +2,29 @@ from flask import Flask, render_template, request, redirect, flash, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash #hashes a password and verifies if a password matches the hash 
+from flask_jwt_extended import JWTManager #manages jwt for auth
+from flask_jwt_extended import create_access_token # creates jwt for user authentication 
+from flask_jwt_extended import jwt_required # ensures that the request has a valid jwt 
+from flask_jwt_extended import get_jwt_identity #identifies the current user from the jwt   
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 import os
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(16).hex()
-app.config['JWT_SECRET_KEY'] = os.urandom(16).hex()
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'
+
 jwt = JWTManager(app)
 
-# Connect to the database
-try:
-    conn = sqlite3.connect("db.sqlite3", check_same_thread=False)
-    c = conn.cursor()
-except sqlite3.Error as e:
-    print(f"Error connecting to the database: {e}")
-    exit(1)
+# Create a connection pool with PostgreSQL
+DATABASE_URL = 'postgresql://parsa:parsa123@localhost/mydb.db'
+engine = create_engine(DATABASE_URL)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm): 
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirmation = PasswordField('Confirm Password', validators=[
@@ -29,7 +33,7 @@ class RegistrationForm(FlaskForm):
     ])
     submit = SubmitField('Register')
 
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm): 
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
@@ -41,19 +45,26 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit():  
         email = form.email.data
         password = form.password.data
         confirmation = form.confirmation.data
 
-        if c.execute("SELECT * FROM users WHERE email=?", (email,)).fetchall():
-            flash('User already registered')
-        else:
-            passhash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
-            c.execute('INSERT INTO users (email, password) VALUES (?, ?)', (email, passhash))
-            conn.commit()
-            flash('Registered successfully')
-            return redirect('/login')
+        # create a session from the session maker
+        session = db_session()
+
+        try:
+            user = session.execute("SELECT * FROM users WHERE email=:email", {"email": email}).fetchone()  
+            if user:  
+                flash('User already registered')
+            else:
+                passhash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8) 
+                session.execute('INSERT INTO users (email, password) VALUES (:email, :password)', {"email": email, "password": passhash}) 
+                session.commit()
+                flash('Registered successfully')
+                return redirect('/login')
+        finally:
+            session.close() 
 
     return render_template('register.html', form=form)
 
@@ -64,17 +75,22 @@ def login():
         email = form.email.data
         password = form.password.data
 
-        user = c.execute("SELECT * FROM users WHERE email=?", (email,)).fetchone()
+        session = db_session()
 
-        if user is None:
-            flash('User not found')
-        else:
-            passhash = user[2]
-            if not check_password_hash(passhash, password):
-                flash('Wrong password')
+        try:
+            user = session.execute("SELECT * FROM users WHERE email=:email", {"email": email}).fetchone()
+
+            if user is None:
+                flash('User not found')
             else:
-                access_token = create_access_token(identity=user[0])
-                return redirect('/protected')
+                passhash = user[2]
+                if not check_password_hash(passhash, password):
+                    flash('Wrong password')
+                else:
+                    access_token = create_access_token(identity=user[0])
+                    return redirect('/protected')
+        finally:
+            session.close()  
 
     return render_template('login.html', form=form)
 
